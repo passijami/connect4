b2c0e5e20c04c9fcf17f737e6ebdcdef30870451
jamip <155644993+passijami@users.noreply.github.com>
2026-09-19 20:35:36 +0300

    update board

--- src/connect4/board.py
+++ src/connect4/board.py
@@ -9,18 +9,30 @@ COLS = 7
 
 
 class Board:
-    """Connect4-lauta.
-
-    TODO: valitaan sisäinen esitys. Yksinkertaisin on 2D-lista (rivi x
-    sarake). Yksinkertainen, optimoi myöhemmin.
+    """Connect4-pelilauta.
+ 
+    Sisäinen esitys self.grid on lista, jossa on ROWS listaa (rivi 0 = ylin rivi),
+    kukin pituudeltaan COLS. Arvot: 0 = tyhjä, 1 = pelaaja 1, 2 = pelaaja 2.
+    self.current_player kertoo kumman pelaajan vuoro on.
+    self.last_move on (rivi, sarake) tai None. Tämä on oleellinen tehokkaaseen
+    voitontarkistukseen!
+ 
+    Käytän aluksi tarkoituksella yksinkertaista listapohjaista esitystä.  
+    Jos myöhemmin käy ilmi, että laudan käsittely hidastaa tekoälyn  
+    hakua merkittävästi, voin harkita bittilaudan käyttämistä.  
+    En kuitenkaan halua optimoida rakennetta ennen kuin siihen on oikeasti tarvetta.
     """
 
     def __init__(self):
-        """Alusta tyhjä lauta ja vuorossa oleva pelaaja. TODO."""
+        """Alusta tyhjä lauta ja vuorossa oleva pelaaja.  
+        TODO: self.grid = [[0] * COLS for _ in range(ROWS)]
+              self.current_player = 1
+              self.last_move = None
+        """
         raise NotImplementedError
 
     def legal_moves(self) -> list[int]:
-        """Palauttaa sarakkeet, joihin voi pudottaa merkin.
+        """Palauttaa sarakkeet (0..COLS-1), joihin voi pudottaa merkin.
 
         TODO.
         """
